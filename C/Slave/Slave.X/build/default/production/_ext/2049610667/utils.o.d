1c8772270233a88eec1214084c9c844299991f86
Wouter <thys_wouter@hotmail.com>
2018-06-06 16:50:05 +0200

    created master c project

---
+++ C/Slave/Slave.X/build/default/production/_ext/2049610667/utils.o.d
@@ -0,0 +1,3 @@
+build/default/production/_ext/2049610667/utils.o:  \
+../../Common/utils.c  \
+ ../../Common/utils.h 
---
+++ C/Slave/Slave.X/build/default/production/_ext/308020537/SYSTEM_Driver.o.d
@@ -0,0 +1,3 @@
+build/default/production/_ext/308020537/SYSTEM_Driver.o:  \
+ ../../Common/Drivers/SYSTEM_Driver.c  \
+ ../../Common/Drivers/SYSTEM_Driver.h 
